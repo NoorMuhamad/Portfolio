93145574950fd9686348f59ba21cfeea1e93eb86
NoorMuhammad <noorrasheed317@gmail.com>
2022-05-25 23:31:14 +0500

    remove unwanted files

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,13 +3,12 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/NOORRASHEED_CV_MERN.pdf";
+import Resume from "../../Assets/resume.png";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -35,9 +34,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <img alt="resume_image" src={Resume}/>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
